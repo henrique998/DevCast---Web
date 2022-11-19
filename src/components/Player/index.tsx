89662e9ae1f73beb8cf8415c3767310cd1325d68
carlos henrique monteiro de moraes <henriquemonteiro037@gmail.com>
2022-11-19 16:57:01 +0000

    component: player created

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,9 +1,90 @@
-import { PlayerContainer } from "./styles";
+import Image from "next/image"
+import { HandsClapping, Pause, Play, Repeat, Shuffle, SkipBack, SkipForward, Star } from "phosphor-react"
+import { useState } from "react"
+
+import { 
+    PrimaryButtonsContainer, 
+    SecondaryButtonsContainer, 
+    EpisodeDetailsContainer, 
+    PlayerContainer, 
+    SliderContainer, 
+    SecondaryControllButton, 
+    SkipButton,
+    SliderRoot,
+    SliderTrack,
+    SliderRange,
+    SliderThumb
+} from "./styles"
 
 export function Player() {
+   const isPlaying = true 
+
    return (
     <PlayerContainer>
-        <h3>Player</h3>
+        <EpisodeDetailsContainer>
+            <Image 
+                src="/card-image.png"
+                alt=""
+                width={150}
+                height={150}
+            /> 
+
+            <div className="texts">
+                <h3 title="A vida é boa">
+                    A vida é boa
+                </h3>
+
+                <span title="Tiago, Diego e Pellizzetti">
+                    Tiago, Diego e Pellizzetti
+                </span>
+            </div>
+        </EpisodeDetailsContainer>
+
+        <PrimaryButtonsContainer>
+            <SkipButton disabled>
+                <SkipBack size={32} weight="fill" />
+            </SkipButton>
+
+            <button className="play-button">
+                {isPlaying ? <Pause size={32} weight="fill" /> : <Play size={32} weight="fill" />}
+            </button>
+
+            <SkipButton>
+                <SkipForward size={32} weight="fill" />
+            </SkipButton>
+        </PrimaryButtonsContainer>
+
+        <SliderContainer>
+            <span>12:02</span>
+
+            <SliderRoot>
+                <SliderTrack>
+                    <SliderRange />
+                </SliderTrack>
+
+                <SliderThumb />
+            </SliderRoot>
+
+            <span>1:35:18</span>
+        </SliderContainer>
+
+        <SecondaryButtonsContainer>
+            <SecondaryControllButton isActive={false}>
+                <Shuffle size={28} weight="fill" />
+            </SecondaryControllButton>
+
+            <SecondaryControllButton isActive>
+                <Repeat size={28} weight="fill" />
+            </SecondaryControllButton>
+
+            <SecondaryControllButton isActive={false}>
+                <Star size={28} weight="fill" />
+            </SecondaryControllButton>
+
+            <SecondaryControllButton isActive>
+                <HandsClapping size={28} weight="fill" />
+            </SecondaryControllButton>
+        </SecondaryButtonsContainer> 
     </PlayerContainer>
    )
 }
--- src/components/Player/styles.ts
+++ src/components/Player/styles.ts
@@ -1,19 +1,202 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components"
+import * as Slider from "@radix-ui/react-slider"
 
 export const PlayerContainer = styled.div`
     max-width: 70rem;
     width: 100%;
-    height: 6.25rem;
+    /* height: 6.25rem; */
 
     margin-left: 18.5rem;
-    /* margin-right: auto; */
 
     background-color: ${props => props.theme.gray200};
     border-radius: 6px;
+    padding: 1.125rem 1rem;
+
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
 
     position: fixed;
     left: 1.875rem;
     bottom: 1.875rem;
     right: 1.875rem;
     z-index: 100;
+`
+
+export const EpisodeDetailsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    img {
+        width: 3.125rem;
+        height: 3.125rem;
+        object-fit: cover;
+        border-radius: 6px;
+    }
+
+    h3 {
+        font-size: 1.125rem;
+        font-weight: 500;
+
+        max-width: 10.375rem;
+        width: 100%;
+
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    span {
+        display: block;
+        margin-top: 0.5rem;
+
+        font-size: 0.875rem;
+        font-weight: 500;
+
+        color: ${props => props.theme.gray600};
+
+        max-width: 10.375rem;
+        width: 100%;
+
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+`
+
+export const PrimaryButtonsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+
+    .play-button {
+        width: 3.125rem;
+        height: 3.125rem;
+        border-radius: 50%;
+        background-color: ${props => props.theme.purple};
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        transition: filter 0.2s;
+    
+        &:hover {
+            svg {
+                filter: brightness(0.85);
+            }
+        }
+    }
+`
+
+export const SkipButton = styled.button`
+    svg {
+        color: ${props => props.theme.gray600};
+    }
+
+    &:disabled {
+        svg {
+            color: ${props => props.theme.gray300};
+            cursor: not-allowed;
+        }
+    }
+
+    transition: filter 0.2s;
+
+    &:not(:disabled):hover {
+        svg {
+            filter: brightness(0.85);
+        }
+    }
+`
+
+export const SliderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.875rem;
+
+    user-select: none;
+
+    > span {
+        font-size: 0.875rem;
+    }
+`
+
+export const SliderRoot = styled(Slider.Root)`
+    position: relative;
+
+    display: flex;
+    align-items: center;
+
+    width: 23rem;
+    height: 4px;
+` 
+
+export const SliderTrack = styled(Slider.Track)`
+    position: relative;
+
+    background-color: ${props => props.theme.gray300};
+    height: 4px;
+    cursor: pointer;
+
+    flex: 1;
+`
+
+export const SliderRange = styled(Slider.Range)`
+    position: absolute;
+
+    background-color: ${props => props.theme.purple};
+    height: 100%;
+
+    cursor: pointer;
+`
+
+export const SliderThumb = styled(Slider.Thumb)`
+    display: block;
+
+    width: 0.75rem;
+    height: 0.75rem;
+    
+    border-radius: 50%;
+    background-color: ${props => props.theme.gray100};
+    
+    &:hover {
+        cursor: grab;
+    } 
+
+    &:focus {
+        cursor: grabbing;
+    } 
+`
+
+export const SecondaryButtonsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+`
+
+interface SecondaryControllButtonProps {
+    isActive: boolean
+}
+
+export const SecondaryControllButton = styled.button<SecondaryControllButtonProps>`
+    svg {
+        color: ${props => props.theme.gray300};
+    }
+
+    ${props => props.isActive && css`
+        svg {
+            color: ${props => props.theme.purple};
+        }
+
+        transition: filter 0.2s;
+    
+        &:hover {
+            svg {
+                filter: brightness(0.85);
+            }
+        }
+    `}
+
 `
